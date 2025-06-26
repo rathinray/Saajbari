5caad48ebd4f112da6b26760bda259b1ceda92b5
Rathin <rathinraybaul@gmail.com>
2025-06-26 20:53:32 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-        <App />
-   
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
